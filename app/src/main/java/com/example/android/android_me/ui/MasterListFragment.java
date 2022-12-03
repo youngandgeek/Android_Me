19083705e9a6d38889a78a8265d3ba950fc46920
eman-essam <eng.emanessam@gmail.com>
2022-12-04 01:52:06 +0200

    MasterListFragment MasterListFragment.java

--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -1,36 +1,38 @@
 package com.example.android.android_me.ui;
 
-import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
-import com.example.android.android_me.ui.MasterListAdapter;
 
 public class MasterListFragment extends Fragment {
 
     public MasterListFragment() {
     }
-
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
+        final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
 
-        View view = inflater.inflate(R.layout.fragment_master_list,container,false);
-        GridView gridView = (GridView) view.findViewById(R.id.grid_view_img);
+        // Get a reference to the GridView in the fragment_master_list xml layout file
+        GridView gridView = (GridView) rootView.findViewById(R.id.grid_view_img);
+
         // Create the adapter
         // This adapter takes in the context and an ArrayList of ALL the image resources to display
         MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
-        gridView.setAdapter(mAdapter);
-        return view;
 
+        // Set the adapter on the GridView
+        gridView.setAdapter(mAdapter);
+
+        // Return the root view
+        return rootView;
     }
 }
 
